19401c9f7e1fb9225d8f9f4334b4cc5cbc2f65ba
Shaw1162084 <1162084@qq.com>
2025-08-04 19:27:31 +0800

    设置偏好,删除不需要的app

--- playground/src/preferences.ts
+++ playground/src/preferences.ts
@@ -11,4 +11,19 @@ export const overridesPreferences = defineOverridesPreferences({
     enableCheckUpdates: false,
     name: import.meta.env.VITE_APP_TITLE,
   },
+  theme: {
+    mode: 'light',
+    /** 是否开启半深色header（只在theme='light'时生效） */
+    semiDarkHeader: true,
+    /** 是否开启半深色菜单（只在theme='light'时生效） */
+    semiDarkSidebar: true,
+  },
+  breadcrumb: {
+    /** 面包屑首页图标是否可见 */
+    showHome: true,
+    /** 面包屑图标是否可见 */
+    showIcon: true,
+    /** 面包屑风格 */
+    styleType: 'background',
+  },
 });
